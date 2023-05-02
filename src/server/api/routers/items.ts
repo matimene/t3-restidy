7e180de42aa81f89a5456336b4b99af4313a149c
Matias <meneghettimatias@gmail.com>
2023-05-02 18:33:52 +0200

    appending user & store to context

--- src/server/api/routers/items.ts
+++ src/server/api/routers/items.ts
@@ -7,13 +7,15 @@ export const itemsRouter = createTRPCRouter({
   getAll: publicProcedure
     .input(z.object({ code: z.string() }))
     .query(async ({ ctx, input }) => {
-      const store = await ctx.prisma.store.findUnique({
-        where: { code: input.code },
-      });
-      if (!store) throw new TRPCError({ code: "NOT_FOUND" });
+      const storeId = ctx.store?.id;
+      if (!storeId)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Store not found",
+        });
 
       const items = await ctx.prisma.item.findMany({
-        where: { storeId: store.id }, // TODO: get store from subdomain
+        where: { storeId },
       });
       return items;
     }),
